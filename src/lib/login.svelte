089d67b02ddf7c511615bfcc43e0f3ca2b51eb28
ColonelKai <kayraacar@hotmail.co.jp>
2023-05-11 17:21:14 +0300

    more steaming poo

--- src/lib/login.svelte
+++ src/lib/login.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import { currentUser, pb } from "./pocketbase";
+  let accselection_login: boolean;
   let username: string;
   let password: string;
   let email: string; 
   async function login() {
     await pb.collection("users").authWithPassword(username, password);
   }
-  async function signUp() {
+  async function register() {
 
     try {
       const data = {
@@ -19,13 +20,14 @@
       const createdUser = await pb.collection('users').create(data);
     }
     catch {
-        // handle error
+        // TODO handle error
     }
 
   }
   function signOut() {
     pb.authStore.clear();
   }
+
 </script>
 
 {#if $currentUser}
@@ -35,21 +37,53 @@
   </p>
 {:else}
 
+  <!--LOGIN / REGISTER SELECTOR-->
+  <form>
+    <input type="radio" id="register" name="accselection" value={false} bind:group={accselection_login}>
+    <label for="register">Register</label><br>
+    <input type="radio" id="login" name="accselection" value={true} bind:group={accselection_login}>
+    <label for="login">Login</label><br>
+  </form>
+  {#if accselection_login}
+  <!--LOGIN-->
   <form>
     <input
       placeholder = "Username"
       type="text"
       bind:value={username}
+      required
     />
-
     <input
       placeholder = "Password"
       type="password"
       bind:value={password}
+      required
     />
-
     <button on:click={login}>Login</button>
   </form>
-
+  {:else}
+  <!--REGISTER-->
+  <form>
+    <input
+      placeholder = "Email"
+      type="email"
+      bind:value={email}
+      required
+    />  
+    <input
+      placeholder = "Username"
+      type="text"
+      bind:value={username}
+      required
+    />
+    <input
+      placeholder = "Password"
+      type="password"
+      bind:value={password}
+      required
+    />
+    <button on:click={register}>Register</button>
+  </form>
+  {/if}
 
 {/if}
