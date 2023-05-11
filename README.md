f1d98ec7e1258fad9668bf32f711efeee9953af5
ColonelKai <kayraacar@hotmail.co.jp>
2023-05-11 16:41:08 +0300

    start of somethingg smelly

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # stratham-mail-registrat-service
+![alt text](https://i.imgur.com/ef85od7.png)
+
+Project for Server BusinessCraft,
+
+A service to allow citizens of Stratham to easily exchange recorded, verifiable mail, and sign them if needed.
+
+(Real Life) Copyright (c) Stetsed, Colonel_Kai 2023
+(Fictional) Copyright (c) Colonel Holdings Corporation
--- src/lib/login.svelte
+++ src/lib/login.svelte
@@ -2,8 +2,26 @@
   import { currentUser, pb } from "./pocketbase";
   let username: string;
   let password: string;
+  let email: string; 
   async function login() {
     await pb.collection("users").authWithPassword(username, password);
+  }
+  async function signUp() {
+
+    try {
+      const data = {
+        username,
+        password,
+        passwordConfirm: password,
+        email 
+      }
+
+      const createdUser = await pb.collection('users').create(data);
+    }
+    catch {
+        // handle error
+    }
+
   }
   function signOut() {
     pb.authStore.clear();
@@ -16,39 +34,22 @@
     <button on:click={signOut}>Sign Out</button>
   </p>
 {:else}
-  <div class="relative flex flex-col justify-center h-96 overflow-hidden">
-    <form on:submit|preventDefault>
-      <div class="form-control w-full max-w-xs m-auto">
-        <label class="label-text" for="username"> Username </label>
-        <input
-          class="input input-bordered w-full max-w-xs"
-          id="username"
-          type="text"
-          placeholder="Username"
-          bind:value={username}
-          required
-        />
-      </div>
-      <div class="form-control w-full max-w-xs m-auto">
-        <label class="label-text" for="password"> Password </label>
-        <input
-          class="input input-bordered w-full max-w-xs"
-          id="password"
-          type="password"
-          placeholder="Password"
-          bind:value={password}
-          required
-        />
-      </div>
-      <div class="form-control w-full max-w-xs m-auto">
-        <button
-          class="btn btn-md sm:btn-sm md:btn-md lg:btn-lg rounded"
-          type="submit"
-          on:click={login}
-        >
-          Login
-        </button>
-      </div>
-    </form>
-  </div>
+
+  <form>
+    <input
+      placeholder = "Username"
+      type="text"
+      bind:value={username}
+    />
+
+    <input
+      placeholder = "Password"
+      type="password"
+      bind:value={password}
+    />
+
+    <button on:click={login}>Login</button>
+  </form>
+
+
 {/if}
