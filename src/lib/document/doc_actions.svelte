eeaa7ceb468197e0b57217d8f49361b3ae534bd1
Colonel Kai <kayraacar@hotmail.co.jp>
2023-05-22 11:29:41 +0300

    Notification Box on Doc Actions

--- src/lib/document/doc_actions.svelte
+++ src/lib/document/doc_actions.svelte
@@ -6,15 +6,23 @@
     import { error } from "@sveltejs/kit";
 
     export let document: Record;
+    
+    let notification: string | null = null;
 
-    function notify(string: String) {
-        console.log(string)
+    function notify(string: string | null) {
+        notification = string;
     }
 
 
 </script>
 
 <div class="flex flex-col p-5 space-y-4">
+
+    {#if notification != null}
+        <div class="bg-red-600 border-red-900 rounded text-white text-center font-bold p-5 animate-fadeIn anima">
+            {notification}
+        </div>
+    {/if}
 
     <Sign document={document} notify={notify} />
 
